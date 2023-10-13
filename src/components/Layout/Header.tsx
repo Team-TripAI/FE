e5e25e393b134c804583ac136dfb6b93b50bfb68
jiahnpak <127730558+jiahnpak@users.noreply.github.com>
2023-10-13 22:31:49 +0900

    feat:      MainPage

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -5,13 +5,12 @@ import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
+// import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import { cyan } from "@mui/material/colors";
 
 const pages = ["예산으로 찾기", "사진으로 찾기"];
 const settings = ["마이페이지", "쓴 글 목록", "로그아웃"];
@@ -40,92 +39,32 @@ const Header = () => {
   };
 
   return (
-    <AppBar position="fixed" sx={{ bgcolor: cyan[500] }}>
+    <AppBar position="fixed" sx={{ bgcolor: "white" }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          {/* xs :none => 화면 클때 xs:flex => 작을때 */}
-
-          {/* 화면 작을때 메뉴바 */}
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography fontFamily="monospace" textAlign="center">
-                    {page}
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          {/* 화면 작을때 LOGO */}
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            TripAI
-          </Typography>
           {/* 화면 클때 메뉴바 펼침 */}
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
-                sx={{ color: "white", display: "block" }}
+                sx={{ color: "black", display: "block" }}
               >
                 {page}
               </Button>
             ))}
           </Box>
           {/* 화면 클때 LOGO */}
-          <Box sx={{ flexGrow: 1.35, display: { xs: "none", md: "flex" } }}>
+          <Box sx={{ flexGrow: 1.3, display: { xs: "none", md: "flex" } }}>
             <Typography
-              variant="h4"
+              variant="h5"
               noWrap
               component="a"
               sx={{
                 fontFamily: "initial",
-                fontStyle: "italic",
                 fontWeight: 500,
                 letterSpacing: ".3rem",
-                color: "inherit",
+                color: "black",
                 textDecoration: "none",
               }}
             >
@@ -139,7 +78,7 @@ const Header = () => {
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <AccountCircleIcon
                   fontSize="large"
-                  sx={{ color: "white" }}
+                  sx={{ color: "black" }}
                 ></AccountCircleIcon>
               </IconButton>
             </Tooltip>
