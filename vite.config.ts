11daa816f8ee0d345c54e6a7aa8e094d4e9a7765
dongyun <0902hdy@gmail.com>
2023-12-10 20:14:55 +0900

    fix: package.json version

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    //base
+    base: '/',
     plugins: [react()],
     define: {
         global: 'globalThis',
+    },
+    optimizeDeps: {
+        disabled: false,
     },
 });
