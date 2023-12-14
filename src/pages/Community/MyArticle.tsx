2df8a6c457aa1c1993cbfd8e29e60a6e3c0715b5
jiahnpak <127730558+jiahnpak@users.noreply.github.com>
2023-12-14 15:55:58 +0900

    feat:delte article

--- src/pages/Community/MyArticle.tsx
+++ src/pages/Community/MyArticle.tsx
@@ -10,9 +10,10 @@ import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../apis/axiosInstance";
 import { useState, useEffect } from "react";
 import Pagination from "@mui/material/Pagination";
+import { Button } from "@mui/material";
 
 interface ArticleInterface {
-  articleId: string;
+  articleId: number;
   title: string;
   locationName: string;
   formattedAddress: string;
@@ -31,6 +32,8 @@ export default function MyArticle() {
   const handleChange = (_event: React.ChangeEvent<unknown>, value: number) => {
     setPageNumber(value);
   };
+  const [deleteDisplayButton, setDeleteDisplayButton] =
+    useState<boolean>(false);
 
   useEffect(() => {
     const getArticles = async () => {
@@ -51,6 +54,20 @@ export default function MyArticle() {
     getArticles();
   }, [pageNumber]);
 
+  const deleteDisplay = () => {
+    setDeleteDisplayButton(!deleteDisplayButton);
+  };
+
+  const deleteArticle = async (_e, articleId: number) => {
+    try {
+      const response = await axiosInstance.delete(`/articles/${articleId}`);
+      console.log(response);
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <Wrapper>
       <Container sx={{ height: "75vh", mt: 13 }}>
@@ -65,38 +82,75 @@ export default function MyArticle() {
             내가 작성한 게시글
           </Typography>
         </Box>
+        <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+          {deleteDisplayButton ? (
+            <Button onClick={deleteDisplay}>취소하기</Button>
+          ) : (
+            <Button onClick={deleteDisplay}>게시글 삭제하기</Button>
+          )}
+        </Box>
         <Box sx={{ display: "flex", justifyContent: "flex-end", p: 3 }}>
           {/* <PostButton onClick={() => navigate('/community/post')}>경험 공유하기</PostButton> */}
         </Box>
         <Grid container spacing={10}>
           {articleList.map((article: ArticleInterface) => (
             <Grid item key={article.articleId} xs={12} sm={6} md={3}>
-              <Card
-                sx={{
-                  width: "100%",
-                  height: "100%",
-                  display: "flex",
-                  flexDirection: "column",
-                }}
-                onClick={() => navigate(`/community/${article.articleId}`)}
-              >
-                <CardMedia
-                  component="div"
+              {deleteDisplayButton ? (
+                <Card
                   sx={{
-                    // 16:9
-                    pt: "56.25%",
+                    width: "100%",
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
                   }}
-                  image={article.image}
-                />
-                <CardContent sx={{ flexGrow: 1 }}>
-                  <Typography gutterBottom fontSize="15px">
-                    {article.title}
-                  </Typography>
-                  <Typography fontFamily="mono-space">
-                    {article.locationName}
-                  </Typography>
-                </CardContent>
-              </Card>
+                >
+                  <CardMedia
+                    component="div"
+                    sx={{
+                      // 16:9
+                      pt: "56.25%",
+                    }}
+                    image={article.image}
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom fontSize="15px">
+                      {article.title}
+                    </Typography>
+                    <Typography fontFamily="mono-space">
+                      {article.locationName}
+                    </Typography>
+                  </CardContent>
+                  <Button onClick={(e) => deleteArticle(e, article.articleId)}>
+                    Delete
+                  </Button>
+                </Card>
+              ) : (
+                <Card
+                  sx={{
+                    width: "100%",
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <CardMedia
+                    component="div"
+                    sx={{
+                      // 16:9
+                      pt: "56.25%",
+                    }}
+                    image={article.image}
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom fontSize="15px">
+                      {article.title}
+                    </Typography>
+                    <Typography fontFamily="mono-space">
+                      {article.locationName}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              )}
             </Grid>
           ))}
         </Grid>
